846e771b3018f398eed4e947e8e5e6f7e31dc369
yzatsepi <01174893@pw.edu.pl>
2023-01-24 20:39:13 +0100

    Proper logout function

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,33 @@
-function successfullyLogout(){
-    alert("You have been successfully logged out");
+function successfullyLogout() {
+    alert("Zostałeś wylogowany.");
 }
+
+function newClientSuccess() {
+    alert("Nowy klient został dodany do bazy danych.");
+}
+
+function newAtrakcjaSuccess() {
+    alert("Nowa atrakcja została dodana do bazy danych.");
+}
+
+function clientEditSuccess() {
+    alert("Dane klienta zostały zmienione pomyślnie.");
+}
+
+function atrakcjaEditSuccess() {
+    alert("Dane atrakcji zostały zmienione pomyślnie.");
+}
+
+function clientDeletionSuccess() {
+    alert("Klient został usunięty z bazy danych.");
+}
+
+function atrakcjaDeletionSuccess() {
+    alert("Atrakcja została usunięta z bazy danych.");
+}
+
+
+function purchaseSuccess() {
+    alert("Bilet został kupiony. Dziękujemy i czekamy na Ciebie w naszym parku!");
+}
+
